5d713721043e8af2413e648e822d5ca8daa5cc43
everr <everrwsr@outlook.com>
2023-01-04 15:24:09 +0800

    条件查询

--- src/main/java/com/everr/mapper/BrandMapper.java
+++ src/main/java/com/everr/mapper/BrandMapper.java
@@ -2,8 +2,10 @@ package com.everr.mapper;
 
 
 import com.everr.pojo.Brand;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,6 +15,25 @@ import java.util.List;
  * Time: 20:48
  */
 public interface BrandMapper {
+    //查询所有
     public List<Brand> selectAll();
+
+    /**
+     * id 查询
+     * @param id
+     * @return
+     */
+    Brand selectById(int id);
+
+    /**
+     * 条件查询
+     * @param status
+     * @param companyName
+     * @param brandName
+     * @return
+     */
+    //List<Brand> selectByCondition(@Param("status") int status, @Param("companyName") String companyName,@Param("brandName") String brandName);
+//    List<Brand> selectByCondition(Brand brand);
+    List<Brand> selectByCondition(Map map);
 
 }
--- src/test/com/everr/MybatisTest.java
+++ src/test/com/everr/MybatisTest.java
@@ -12,7 +12,9 @@ import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * description :
@@ -21,7 +23,7 @@ import java.util.List;
  * @date: 2022/12/30 21:48
  **/
 public class MybatisTest {
-@Test
+    @Test
     public void testSelectAll() throws IOException {
         //1、加载mybatis配置文件，
         String resource = "mybatis-config.xml";
@@ -42,4 +44,73 @@ public class MybatisTest {
         //释放
         sqlSession.close();
     }
+
+    @Test
+    public void testSelectById() throws IOException {
+        int id = 1;
+        //1、加载mybatis配置文件，
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+
+        //2获取sqlSession 对象，用它执行sql
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+
+        // 3. 执行sql
+        //List<User> users = sqlSession.selectList("test.selectAll");
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+        Brand brand = brandMapper.selectById(id);
+
+        System.out.println(brand);
+        //释放
+        sqlSession.close();
+    }
+
+    @Test
+    public void testSelectByCondition() throws IOException {
+        //接受参数
+        int status = 1;
+        String companyName = "华为";
+        String brandName = "华为";
+        //处理参数
+        companyName = "%" + companyName + "%";
+        brandName = "%" + brandName + "%";
+
+////封装对象
+//        Brand brand = new Brand();
+//        brand.setStatus(status);
+//        brand.setCompanyName(companyName);
+//        brand.setBrandName(brandName);
+
+        Map map = new HashMap();
+        map.put("status",status);
+        map.put("companyName",companyName);
+        map.put("brandName",brandName);
+
+        //1、加载mybatis配置文件，
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+
+        //2获取sqlSession 对象，用它执行sql
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        // 3. 执行sql
+
+        //List<User> users = sqlSession.selectList("test.selectAll");
+        //获取mapper接口的代理对象
+
+        //执行方法
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+        List<Brand> brands = brandMapper.selectByCondition(map);
+
+
+        System.out.println(brands);
+        //释放
+        sqlSession.close();
+    }
+
 }
